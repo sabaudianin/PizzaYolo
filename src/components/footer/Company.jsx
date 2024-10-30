1c7da7ff1e5b93776633eff2acb5d9ab15508f36
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 <sabaudianin@gmail.com>
2024-10-30 17:07:27 +0100

    dependencies update

---
+++ src/components/footer/Company.jsx
@@ -0,0 +1,25 @@
+export const Company = () => {
+	return (
+		<section>
+			<div>
+				<p>boobec@vp.pl</p>
+				<a
+					className="hover:text-gray-600 font-bold pr-2"
+					href="https://github.com/sabaudianin"
+					target="_blank"
+					rel="noopener noreferrer"
+				>
+					A<i className="fa-brands fa-github text-2xl"></i>
+				</a>
+				<a
+					className="text-blue-600 hover:text-blue-800 font-bold"
+					href="https://linkedin.com/in/rafal-bobko-569388234"
+					target="_blank"
+					rel="noopener noreferrer"
+				>
+					<i className="fa-brands fa-linkedin text-2xl mx-2"></i>
+				</a>
+			</div>
+		</section>
+	);
+};
